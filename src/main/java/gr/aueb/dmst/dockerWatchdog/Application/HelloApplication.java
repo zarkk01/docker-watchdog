8c9535c9b2e18592a979b1b3359f3358b387fa3b
Giannis Tampakis <giannistampakis04@gmail.com>
2023-12-26 13:13:39 +0200

    test

---
+++ src/main/java/gr/aueb/dmst/dockerWatchdog/Application/HelloApplication.java
@@ -0,0 +1,22 @@
+package gr.aueb.dmst.dockerWatchdog.Application;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("containersScene.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 920, 540);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
