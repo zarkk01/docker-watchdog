226f302a1518b5aaf0e33728e3012d04aebb06b4
= <zarkalhs@gmail.com>
2023-12-26 14:42:16 +0200

    Make top bar to be an all scenes

--- src/main/java/gr/aueb/dmst/dockerWatchdog/Application/HelloApplication.java
+++ src/main/java/gr/aueb/dmst/dockerWatchdog/Application/HelloApplication.java
@@ -12,7 +12,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/containersScene.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/imagesScene.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 920, 540);
         stage.setScene(scene);
         stage.setTitle("Docker WatchDog");
