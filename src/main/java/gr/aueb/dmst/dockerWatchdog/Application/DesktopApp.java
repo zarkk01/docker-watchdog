0b534c9c082b2f29b8ea27d9afeacc4fddbdcba9
Giannis Tampakis <giannistampakis04@gmail.com>
2023-12-28 17:54:50 +0200

    Make changes to app styling, make the look for all the scenes

--- src/main/java/gr/aueb/dmst/dockerWatchdog/Application/DesktopApp.java
+++ src/main/java/gr/aueb/dmst/dockerWatchdog/Application/DesktopApp.java
@@ -17,7 +17,7 @@ public class DesktopApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(DesktopApp.class.getResource("/containersScene.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(),1200,700);
+        Scene scene = new Scene(fxmlLoader.load(),1100,700);
         stage.setScene(scene);
         stage.setTitle("Docker Watchdog");
         stage.show();
