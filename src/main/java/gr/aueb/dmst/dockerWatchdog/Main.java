925d27eec238cd1a83d6871096c75d8f6f97a973
zark <zarkalhs@gmail.com>
2024-01-09 11:14:09 +0200

    Finish javadoc support in Main and WebApp

--- src/main/java/gr/aueb/dmst/dockerWatchdog/Main.java
+++ src/main/java/gr/aueb/dmst/dockerWatchdog/Main.java
@@ -7,8 +7,6 @@ import com.github.dockerjava.api.DockerClient;
 import com.github.dockerjava.core.DefaultDockerClientConfig;
 import com.github.dockerjava.core.DockerClientBuilder;
 
-import org.springframework.boot.SpringApplication;
-
 import gr.aueb.dmst.dockerWatchdog.Application.DesktopApp;
 import gr.aueb.dmst.dockerWatchdog.Models.MyImage;
 import gr.aueb.dmst.dockerWatchdog.Models.MyInstance;
@@ -17,33 +15,83 @@ import gr.aueb.dmst.dockerWatchdog.Threads.DatabaseThread;
 import gr.aueb.dmst.dockerWatchdog.Threads.ExecutorThread;
 import gr.aueb.dmst.dockerWatchdog.Threads.MonitorThread;
 
+import org.springframework.boot.SpringApplication;
+
+/**
+ * The main class of the application.
+ * It initializes the Docker client and lists for instances, images, and volumes,
+ * Also, starts the necessary threads for monitoring Docker events,
+ * executing actions on Docker Components, and database operations.
+ * Lastly, launches the JavaFX application and the Spring Boot application
+ * which supports our REST API.
+ */
 public class Main {
-    private static final DefaultDockerClientConfig builder = DefaultDockerClientConfig.createDefaultConfigBuilder()
+
+    /**
+     * Configuration for the Docker client.
+     */
+    private static final DefaultDockerClientConfig builder =
+            DefaultDockerClientConfig.createDefaultConfigBuilder()
             .build();
-    public final static DockerClient dockerClient = DockerClientBuilder.getInstance(builder).build();
+
+    /**
+     * Instance of the Docker client which is used to
+     * communicate with the Docker daemon.
+     */
+    public static final DockerClient dockerClient =
+            DockerClientBuilder.getInstance(builder).build();
+
+    /**
+     * Lists for Docker containers so that we can keep track of them
+     * and then store them in the database.
+     */
     public static ArrayList<MyInstance> myInstances = new ArrayList<>();
+
+    /**
+     * Lists for Docker images so that we can keep track of them
+     * and then store them in the database.
+     */
     public static ArrayList<MyImage> myImages = new ArrayList<>();
+
+    /**
+     * Lists for Docker volumes so that we can keep track of them
+     * and then store them in the database.
+     */
     public static ArrayList<MyVolume> myVolumes = new ArrayList<>();
+
+    /**
+     * Thread for database operations in a static concept so to
+     * be able for usage by MonitorThread.
+     */
     public static Thread dbThread = new Thread(new DatabaseThread());
 
+    /**
+     * Main method of the application.
+     * It starts the Spring Boot application, the JavaFX application,
+     * and the threads for monitoring Docker events, executing actions on Docker Components,
+     * and database operations. It first starts the Spring Boot application so that
+     * the REST API is available before the JavaFX application starts.
+     * @param args command line arguments
+     */
     public static void main(String[] args) {
 
         try {
 
+            // Start Spring Boot application
             SpringApplication.run(WebApp.class, args);
 
-            MonitorThread newDockerMonitor = new MonitorThread();
-            Thread newMonitorThread = new Thread(newDockerMonitor);
+            // Start threads in a specific order so to prevent errors
+            Thread newMonitorThread = new Thread(new MonitorThread());
             newMonitorThread.start();
 
-            ExecutorThread dockerExecutor = new ExecutorThread();
-            Thread executorThread = new Thread(dockerExecutor);
+            Thread executorThread = new Thread(new ExecutorThread());
             executorThread.start();
 
             dbThread.start();
 
             new Thread(DatabaseThread::updateLiveMetcrics).start();
 
+            // Start JavaFX application and GUI displays
             Application.launch(DesktopApp.class, args);
 
         } catch (Exception e) {
--- src/main/java/gr/aueb/dmst/dockerWatchdog/WebApp.java
+++ src/main/java/gr/aueb/dmst/dockerWatchdog/WebApp.java
@@ -3,9 +3,22 @@ package gr.aueb.dmst.dockerWatchdog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * The main entry point for the Spring Boot application.
+ * From here, the application will search for the other
+ * Spring Boot components like Entities, Services and Controllers
+ * and use so to launch. This will start when Main.java is executed
+ * so user can use the application with one click.
+ */
 @SpringBootApplication
-public class WebApp{
-        public static void main(String[] args) {
-            SpringApplication.run(WebApp.class, args);
-        }
+public class WebApp {
+
+    /**
+     * The main method which will be used to launch the Spring Boot application.
+     *
+     * @param args command line arguments
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(WebApp.class, args);
+    }
 }
