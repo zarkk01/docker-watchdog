fecdc56071e0f2cb657832c5d0233add1b7e35d6
zark <zarkalhs@gmail.com>
2024-01-12 16:31:19 +0200

    Add javadoc on ImagesRepository

--- src/main/java/gr/aueb/dmst/dockerWatchdog/Repositories/ImagesRepository.java
+++ src/main/java/gr/aueb/dmst/dockerWatchdog/Repositories/ImagesRepository.java
@@ -3,5 +3,10 @@ package gr.aueb.dmst.dockerWatchdog.Repositories;
 import gr.aueb.dmst.dockerWatchdog.Models.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ * ImagesRepository is an interface that help us with CRUD operations on the Image entity.
+ * ImagesRepository does not declare any methods, as it relies on the standard methods provided by JpaRepository
+ * but if any custom database access methods are needed in the future, they can be added here.
+ */
 public interface ImagesRepository extends JpaRepository<Image, Long> {
 }
